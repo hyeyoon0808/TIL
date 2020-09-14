42e0eaaf7df139988802fdcbc036ffe4974efc48
hyeyoon <hyeyooncho08@gmail.com>
2020-09-15 00:14:37 +0900

    update spring memo

--- practice_algorithm/C++/BJ10799/BJ10799.cpp
+++ practice_algorithm/C++/BJ10799/BJ10799.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
+#include <string>
+#include <stack>
 
-int maint()
+using namespace std;
+
+int main()
 {
+    stack<char> stOpen;
+    string input;
+    cin >> input;
+
+    int result = 0;
+    char preChar;
+    for (auto ch : input)
+    {
+        if (ch == '(')
+        {
+            stOpen.push(ch);
+        }
+        else
+        {
+            if (preChar == '(')
+            {
+                result += stOpen.size() - 1;
+            }
+            else
+            {
+                result++;
+            }
+            stOpen.pop();
+        }
+        preChar = ch;
+    }
+    cout << result << endl;
+    return 0;
 }
---
+++ spring/memo/RESTful-9:14.md
@@ -0,0 +1,14 @@
+# **RESTful**
+
+**2020.September.14**
+
+ 
+
+## RESTful API이란?
+
+
+
+
+
+## JSON이란?
+
