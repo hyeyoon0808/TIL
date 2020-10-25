51e14020f69044a6beebfeff64d12f414ff4578d
hyeyoon0808 <hyeyooncho08@gmail.com>
2020-10-25 19:22:04 +0900

    update 0x03 lesson

---
+++ practice_algorithm/C++/BJ12865/BJ12865.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, k;
+    int w[1000], v[1000];
+    int result[1000][1000];
+    cin >> n >> k;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> w[i] >> v[i];
+    }
+    for (int x = 0; x < n; x++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+                }
+    }
+}
