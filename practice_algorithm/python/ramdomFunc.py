0804ed68617180e5f09a4ab56df3b7d90f21fe07
hyeyoon0808 <hyeyooncho08@gmail.com>
2020-12-12 01:23:35 +0900

    ADD AWS, kubernetes notes

---
+++ practice_algorithm/python/ramdomFunc.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*- 
+
+from random import *
+print(random()) #provide random value between 0.0~1.0 
+print(random() * 10)
+print(int(random() * 10))# 0~10 미만의 임의의 값 생성
+print(int(random() * 10))# 0~10 미만의 임의의 값 생성
+print(int(random() * 10))# 0~10 미만의 임의의 값 생성
+print(int(random() * 10)+1)# 1~10 미만의 임의의 값 생성
+print(int(random() * 10)+1)# 1~10 미만의 임의의 값 생성
+print(int(random() * 10)+1)# 1~10 미만의 임의의 값 생성
+print(int(random() * 10)+1)# 1~10 미만의 임의의 값 생성
+
+print(int(random() * 45)+1) # 1~45
+
+print(randrange(1, 46)) # 1~46 미만의
+
+print(randint(1, 45)) # 1~45 이하의
