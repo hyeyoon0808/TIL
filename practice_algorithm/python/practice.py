ad9d75832cd09a1c61299621d3e3cd9b9f260b7c
hyeyoon0808 <hyeyooncho08@gmail.com>
2020-12-12 00:50:45 +0900

    ADD python practice

---
+++ practice_algorithm/python/practice.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*- 
+
+print(5)
+print(-10)
+print(3.14)
+print('풍선')
+print("ㅋ"*9)  
+
+# 참/거짓
+print( 5 < 10)
+print(5 > 10)
+print(not True)
+print(not(5 > 10))
+
+#변수
+name = "연탄이"
+animal = "강아지"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+#애완동물을 소개해 주세요~
+print("우리집 "+animal+"의 이름은 "+name+"예요")
+print(name+"는 "+str(age)+"살이며, "+hobby+"을 아주 좋아해요")
+print(name, "는 ", age, "살이며, ", hobby, "을 아주 좋아해요")
+print("연탄이는 어른일까요? "+str(is_adult))
+
+# 연산
+print(5*2) #10
+print(2**3) #2^3 = 8
+print(5%3) #나머지 = 2
+print(5//3) # 몫 = 1
+print(10//3) # 3
+print(10 < 3) #false
+print(3 ==3) #true
+print(4==2) # false
+print(3+4 ==7) # true
+
