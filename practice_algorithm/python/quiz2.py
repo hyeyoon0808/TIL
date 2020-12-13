3f479c757c0227aa3ab29108247ab1e1af10ade1
hyeyoon0808 <hyeyooncho08@gmail.com>
2020-12-13 23:46:42 +0900

    ADD quiz2

--- practice_algorithm/python/quiz2.py
+++ practice_algorithm/python/quiz2.py
@@ -6,3 +6,5 @@ my_str = my_str[:my_str.index(".")]
 #my_str[0:5] 0~5직전까지
 print(my_str)
 
+password = my_str[:3] + str(len(my_str))+str(my_str.count("e"))+"!"
+print("{0}의 비밀번호는 {1}입니다.".format(url, password))
