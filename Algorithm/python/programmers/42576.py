bf7ecad35394d5cb5aae49b81a4b4974483e49a3
hyeyoon0808 <hyeyooncho08@gmail.com>
2021-05-03 23:06:06 +0900

    ADD python library

--- Algorithm/python/programmers/42576.py
+++ Algorithm/python/programmers/42576.py
@@ -7,3 +7,10 @@ def solution(participant, completion):
             return par
     
     return participant[-1]
+
+## 다른 사람 풀이
+import collections
+
+def solution(participant, completion):
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
